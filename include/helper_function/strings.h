d2fa0e24b87e41547c0488cd8baf74b197081b8c
Yulin Wu <drm343@gmail.com>
2017-10-18 23:34:04 +0800

    修改 String api 名稱並且註解

---
+++ include/helper_function/strings.h
@@ -0,0 +1,29 @@
+#ifndef HEADER_STRING_HELPER
+#define HEADER_STRING_HELPER
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+
+/** @brief 求出 UTF8 版本的 string 長度
+ * @param str 想求長度的 String 物件
+ * @return 長度
+ */
+size_t String_length(char *str);
+
+
+/** @brief 求出 ascii 版本的 string 長度
+ * @param str 想求長度的 String 物件
+ * @return 長度
+ */
+size_t String_ascii_length(char *str);
+
+
+/** @brief 求出顯示在螢幕所需要的長度
+ * @param str 想求長度的 String 物件
+ * @param full_size 一個全形文字的長度
+ * @return 長度
+ */
+size_t String_width_length(const char *str, int64_t full_size);
+#endif
---
+++ src/helper_function-strings.m
@@ -0,0 +1,89 @@
+#include "helper_function/strings.h"
+
+
+/** @brief 求出 UTF8 版本的 string 長度
+ * @param str 想求長度的 String 物件
+ * @return 長度
+ */
+size_t String_length(char *str) {
+  size_t max_counter = strlen(str);
+  size_t move_position = 0;
+  int64_t len = 0;
+  unsigned char check = 0;
+
+  for (size_t index = 0; index < max_counter; index += move_position) {
+    check = str[index];
+    len++;
+
+    if (check < 0b11000000) {
+      move_position = 1;
+    }
+    else if (check < 0b11100000 && check > 0b11000000) {
+      move_position = 2;
+    }
+    else if (check < 0b11110000 && check > 0b11100000) {
+      move_position = 3;
+    }
+    else if (check > 0b11110000) {
+      move_position = 4;
+    }
+    else if (check == 0) {
+      break;
+    }
+    else {
+      printf("bug\n");
+      break;
+    }
+  }
+  return len;
+}
+
+
+/** @brief 求出 ascii 版本的 string 長度
+ * @param str 想求長度的 String 物件
+ * @return 長度
+ */
+size_t String_ascii_length(char *str) {
+  return strlen(str);
+}
+
+
+/** @brief 求出顯示在螢幕所需要的長度
+ * @param str 想求長度的 String 物件
+ * @param full_size 一個全形文字的長度
+ * @return 長度
+ */
+size_t String_width_length(const char *str, int64_t full_size) {
+  size_t max_counter = strlen(str);
+  size_t index = 0;
+  size_t move_position = 0;
+  int64_t len = 0;
+  unsigned char check = 0;
+
+  for (index = 0; index < max_counter; index += move_position) {
+    check = str[index];
+    len += 2;
+
+    if (check < 0b11000000) {
+      move_position = 1;
+      len--;
+    }
+    else if (check < 0b11100000 && check > 0b11000000) {
+      move_position = 2;
+    }
+    else if (check < 0b11110000 && check > 0b11100000) {
+      move_position = 3;
+    }
+    else if (check > 0b11110000) {
+      move_position = 4;
+    }
+    else if (check == 0) {
+      break;
+    }
+    else {
+      printf("bug\n");
+      break;
+    }
+  }
+  return len * (full_size / 2);
+}
