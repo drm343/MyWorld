65e106ca21780c454df50681f0efa25a6ffad130
Yulin Wu <drm343@gmail.com>
2017-10-20 03:18:23 +0800

    Remove useless header

--- include/graphic.h
+++ include/graphic.h
@@ -4,11 +4,10 @@
 #include <SDL2/SDL.h>
 #include <SDL2/SDL_ttf.h>
 
-#include "helper_function/debug.h"
 
 #include "base_type.h"
 
-#import <Foundation/Foundation.h>
+
 typedef SDL_Texture *SDL_Texture_Access;
 
 typedef struct {
